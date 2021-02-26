bdbd325d94cbd3c77e3ca8dbaa2e5df35632d5d8
rajusingh497 <57539454+rajusingh497@users.noreply.github.com>
2021-02-26 17:31:10 +0530

    new

--- constants/httpStatusCodes.js
+++ constants/httpStatusCodes.js
@@ -1,11 +1,34 @@
+/**
+ * @module httpStatusCodes
+ */
+/**
+ * 200 OK
+ * @type {number}
+ */
 const OK = 200;
+/**
+ * @type {number}
+ */
 const CREATED = 201;
+/**
+ * @type {number}
+ */
 const NO_CONTENT = 204;
-
+/**
+ * @type {number}
+ */
 const BAD_REQUEST = 400;
+/**
+ * @type {number}
+ */
 const UNAUTHORISED = 401;
+/**
+ * @type {number}
+ */
 const NOT_FOUND = 404;
-
+/**
+ * @type {number}
+ */
 const INTERNAL_SERVER_ERROR = 500;
 
 module.exports = {
